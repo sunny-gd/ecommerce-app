83855dafda86c04ec4f00d12d0d6d488640a69f9
sunny-gd <sanichaudhari.idar@gmail.com>
2025-05-04 21:07:25 +0530

    updated baseroute

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 function App() {
   return (
     <Provider store={store}>
-      <Router basename="/ecommerce-app">
+      <Router>
         <div className="App">
           <Navbar />
           <Routes>
