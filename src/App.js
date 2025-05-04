c63fcea1e7716bc9ff25feb6acea8f517c03c8cb
sunny-gd <sanichaudhari.idar@gmail.com>
2025-05-04 18:28:16 +0530

    changed routes

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import './App.css';
 function App() {
   return (
     <Provider store={store}>
-      <Router>
+      <Router basename="/ecommerce-app">
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path="/ecommerce-app" element={<Products />} />
-            <Route path="/ecommerce-app/cart" element={<Cart />} />
-            <Route path="/ecommerce-app/about" element={<About />} />
+            <Route path="/" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
           </Routes>
           <Footer />
         </div>
